b7207678617a7a05160d6bc1d0c57a187ea14169
W0198745JC <w0198745@campus.nscc.ca>
2024-11-07 12:01:32 -0400

    worstchecktechEVER

--- tc4-functions-gradecalc.py
+++ tc4-functions-gradecalc.py
@@ -15,37 +15,63 @@ def main():
     print("All letter grades except F can include a + or - symbol.")
     print("Calculated grade point value cannot exceed 4.0.\n")
 
-    numericGrade = 0.0
+def courseInput (course):
 
-    #Gather user inputs
-    letterGrade = input("Please enter a letter grade : ").upper()
-    modifier = input("Please enter a modifier (+, - or nothing) : ")
 
-    # Determine base numeric value of the grade
-    if letterGrade == "A":
-        numericGrade = 4.0
-    elif letterGrade == "B":
-        numericGrade = 3.0
-    elif letterGrade == "C":
-        numericGrade = 2.0
-    elif letterGrade == "D":
-        numericGrade = 1.0
-    elif letterGrade == "F":
-        numericGrade = 0.0
-    else:
-        #If an invalid entry is made
-        print("You entered an invalid letter grade.")
-    
-    # Determine whether to apply a modifier
-    if modifier == "+":
-        if letterGrade != "A" and letterGrade != "F": # Plus is not valid on A or F
-            numericGrade += 0.3
-    elif modifier == "-":
-        if letterGrade != "F":     # Minus is not valid on F
-            numericGrade -= 0.3
-
+        #Gather user inputs
+        letterGrade = input(f"Please enter a letter grade for {course} : ").upper()
+        modifier = input("Please enter a modifier (+, - or nothing) : ")
+        
+        # Determine base numeric value of the grade
+        if letterGrade == "A":
+             numericGrade = 4.0
+        elif letterGrade == "B":
+            numericGrade = 3.0
+        elif letterGrade == "C":
+            numericGrade = 2.0
+        elif letterGrade == "D":
+            numericGrade = 1.0
+        elif letterGrade == "F":
+            numericGrade = 0.0
+        else:
+                #If an invalid entry is made
+            print("You entered an invalid letter grade.")
+            
+            # Determine whether to apply a modifier
+        if modifier == "+":
+            if letterGrade != "A" and letterGrade != "F": # Plus is not valid on A or F
+                    numericGrade += 0.3
+        elif modifier == "-":
+            if letterGrade != "F":     # Minus is not valid on F
+                    numericGrade -= 0.3
+            return numericGrade
     # Output final message and result, with formatting
-    print("The numeric value is: {0:.1f}".format(numericGrade))
-
+def classValue(course):
+     numericGrade= courseInput
+     print("The numeric value for {1} is : {0:.1f}".format (numericGrade,course))
+     
+       
 #PROGRAM EXECUTION STARTS HERE
 main()
+courseInput("PROG1700")
+print("")
+courseInput("NETW1700")
+print("")
+courseInput("OSYS1200")
+print("")
+courseInput("WEBD1000")
+print("")
+courseInput("COMM1700")
+print("")
+courseInput("DBAS1007")
+print("")
+
+print("********************************************************")
+print("")
+print("")
+classValue("PROG1700")
+classValue("NETW1700")
+classValue("OSYS1200")
+classValue("WEBD1000")
+classValue("COMM1700")
+classValue("DBAS1007")
